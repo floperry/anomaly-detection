39973634ea92ea92245ea9628a00198b06c13fc0
fenng <fengqingsong90@gmail.com>
2018-01-23 20:59:00 +0800

    1-layer denoising auto-encoder with L2

--- autoencoder_with_oneclasssvm.py
+++ autoencoder_with_oneclasssvm.py
@@ -11,7 +11,7 @@ from sklearn.svm import OneClassSVM
 # Load data
 data = sio.loadmat("data/FM/train_data_psd.mat")
 data_train = np.asarray(data['train_data'])
-data = sio.loadmat("data/FM/awgn/testdata_psd_awgn_24dB.mat")
+data = sio.loadmat("data/FM/awgn/testdata_psd_awgn_27dB.mat")
 data_test = np.asarray(data['test_data'])
 
 # Data preprocessing
@@ -22,19 +22,18 @@ x_test = scalar.transform(data_test)
 
 # Network parameters
 num_input = 512
-num_hidden_1 = 64
-num_hidden_2 = 8
+num_hidden_1 = 8
 epochs = 200
 optimizer = optimizers.Adadelta()
+model_autoencoder_path = "model_svm/autoencoder_1Layer_9.h5"
+model_encoder_path = "model_svm/encoder_1Layer_9.h5"
 
 # Define autoencoder
 inputs = Input(shape=(num_input,))
 encoded_1 = Dense(num_hidden_1, activation='relu')(inputs)
-encoded_2 = Dense(num_hidden_2, activation='relu')(encoded_1)
-decoded_1 = Dense(num_hidden_1, activation='relu')(encoded_2)
-decoded_2 = Dense(num_input, activation='linear')(decoded_1)
-autoencoder = Model(inputs=inputs, outputs=decoded_2)
-encoder = Model(inputs=inputs, outputs=encoded_2)
+decoded_1 = Dense(num_input, activation='linear')(encoded_1)
+autoencoder = Model(inputs=inputs, outputs=decoded_1)
+encoder = Model(inputs=inputs, outputs=encoded_1)
 autoencoder.compile(optimizer=optimizer, loss='mse')
 tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
 
@@ -46,6 +45,10 @@ autoencoder.fit(x_train, x_train,
                 verbose=2,
                 callbacks=[tensorboard],
                 validation_data=(x_valid, x_valid))
+
+# Save model
+autoencoder.save(model_autoencoder_path)
+encoder.save(model_encoder_path)
 
 # Testing
 encoded_train = encoder.predict(x_train)
@@ -62,7 +65,7 @@ fpr, tpr, threshold = roc_curve(y_test, score, pos_label=1)
 auc_value = auc(fpr, tpr)
 print(auc_value)
 
-
+'''
 # Scale and visualize the embedding vectors
 def plot_embedding(x, y, title=None):
     # x_min, x_max = np.min(x, 0), np.max(x, 0)
@@ -94,3 +97,5 @@ X_tsne = tsne.fit_transform(X)
 plot_embedding(X_tsne, Y,
                "t-SNE embedding (time %.2fs)" % (time() - t0))
 plt.show()
+'''
+
